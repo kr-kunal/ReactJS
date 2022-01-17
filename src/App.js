52b17ee77438884ba208c69dc54cf73d580713f6
kr-kunal <kunal.p@knackroot.com>
2022-01-17 19:36:26 +0530

    Responsive Navbar With Styled Comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
+import styled from 'styled-components';
 // import NewUserList from './components/WithHooks/NewUserList';
-import DataFetch from './components/WithHooks/DataFetch';
+// import DataFetch from './components/WithHooks/DataFetch';
 // import Counter from './components/WithHooks/Counter';
 // import StateDiff from './components/WithHooks/StateDiff';
 // import UseContext from './components/ContextEx/UseContext';
 // import UserList from './components/UserList';
 // import PostDemo from './components/PostDemo';
+import Navbar from './components/StylesElement/Navbar';
+
+const Container = styled.div`
+  background: #67bc98;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="container h4 mt-3">
+    <div>
+      <Container>
+      <Navbar/>
+      </Container>
       {/* <UserList/> */}
       {/* <PostDemo/> */}
       {/* <NewUserList/>  */}
       {/* <Counter/> */}
       {/* <StateDiff/> */}
       {/* <UseContext/> */}
-      <DataFetch/>
+      {/* <DataFetch/> */}
     </div>
   );
 }
 
 export default App;
+
